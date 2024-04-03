5c55c3cb7645fb141c7eb45ceabf0aa5612c54e1
Samuel Xavier <sancrisxa@gmail.com>
2024-04-02 21:07:14 -0300

    Excecao para CPF ja cadastrado

--- os/src/main/java/com/sancrisxa/os/repositories/TecnicoRepository.java
+++ os/src/main/java/com/sancrisxa/os/repositories/TecnicoRepository.java
@@ -2,9 +2,13 @@ package com.sancrisxa.os.repositories;
 
 import com.sancrisxa.os.domain.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
 
+    @Query("SELECT tecnico FROM Tecnico tecnico WHERE tecnico.cpf =:cpf")
+    Tecnico findByCPF(@Param("cpf") String cpf);
 }
--- os/src/main/java/com/sancrisxa/os/resources/exceptions/ResourceExceptionHandler.java
+++ os/src/main/java/com/sancrisxa/os/resources/exceptions/ResourceExceptionHandler.java
@@ -1,9 +1,35 @@
 package com.sancrisxa.os.resources.exceptions;
 
+import com.sancrisxa.os.exceptions.ObjectNotFoundException;
+import com.sancrisxa.os.exceptions.StandardError;
+import com.sancrisxa.os.service.exceptions.DataIntegratyViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+    /*
+     * Manipulando exceção para objeto não encontrado
+     */
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e) {
+        StandardError error = new StandardError(System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(), e.getMessage());
 
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    /*
+     * Manipulando exceção violação da integridade de dados
+     */
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(DataIntegratyViolationException e) {
+        StandardError error = new StandardError(System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
